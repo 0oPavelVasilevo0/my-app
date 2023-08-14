70dbb448efa543b9b2ff794bbe893b2f90355d21
Pavel Vasilev <karterpahan@gmail.com>
2023-08-14 21:52:36 +0300

    fix localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './../src/styles/App.css';
 import AddPersonForm from './components/AddPersonForm';
 import CombinedList from './components/CombinedList';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  function addPerson(name, age, tel) {
+    setContacts([...contacts, { name, age, tel }]);
+  }
+
+  function deletePerson(updatedContacts) {
+    setContacts(updatedContacts);
+  }
+
+  function editPerson(updatedContacts) {
+    setContacts(updatedContacts);
+  }
+
   return (
-    <div className="App">
-      <AddPersonForm />
-      <CombinedList />
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <CombinedList data={contacts} onDelete={deletePerson} onEdit={editPerson} />
     </div>
   );
 }
